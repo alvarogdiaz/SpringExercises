c49e342e05cf5c5b994791054541367d8f7579f5
Álvaro García Díaz <alvarog.diaz@bosonit.com>
2021-09-14 17:05:53 +0200

    BS3 - Orden ejecución

--- src/main/java/com/bosonit/restservice/BS3/Second.java
+++ src/main/java/com/bosonit/restservice/BS3/Second.java
@@ -1,9 +1,11 @@
 package com.bosonit.restservice.BS3;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(5)
 public class Second implements CommandLineRunner {
 
     @Override
--- src/main/java/com/bosonit/restservice/BS3/Third.java
+++ src/main/java/com/bosonit/restservice/BS3/Third.java
@@ -1,9 +1,11 @@
 package com.bosonit.restservice.BS3;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(1)
 public class Third implements CommandLineRunner {
 
     @Override
